4b22f9c9b45535ea6b276544352f6462b575dcc2
niranjfantain <niranj@fantain.com>
2014-05-13 15:31:58 +0530

    Search and replace done with Node.js

--- bin/substitute.js
+++ bin/substitute.js
@@ -1,27 +1,21 @@
 #! /usr/bin/env node
 
-
-var search = process.argv[2];
+// Gets the Paramters from the fantainbuild to search and replace 
+var search = new RegExp(process.argv[2]);
 var replace = process.argv[3];
 var filename = process.argv[4];
 
-console.log(search);
-console.log(replace);
-console.log(filename);
-
-search=/(fg.env\s*=\s*")(.*)(".*)/
-
+// Initializing the node.js File System Module
 var fs = require('fs');
 
-fs.readFileSync(filename).toString().split('\n').forEach(function (line) { 
-    console.log(line.toString().replace(search, replacer));
-	
-    //fs.appendFileSync($2, line.toString() + "\n");
+// Searches and replace old  with the new  environment 
+fs.readFileSync(filename).toString().split('\n').forEach(function (line) {
+    var test = line.toString().replace(search, replacer);
+    console.log(test);
 });
 
+// Replace Operation to set the new environment
 function replacer(match, m1, m2, m3, offset, string){
-	//console.log (match);
-	//console.log (offset);
-	//console.log (string);
-	return m1 + replace + m3;
+        return m1 + replace + m3;
 }
+
